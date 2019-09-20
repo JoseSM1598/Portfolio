66ab8edb2b21c948343614fe65d1726e594df0fb
js665 <js665@duke.edu>
2019-09-20 18:07:32 -0400

    nothing

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -31,7 +31,7 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner">
                         <div className="desc">
                           <h1 style = {{color:"white"}}>I love building<br /> software!</h1>
-                          <p ><a className="btn btn-primary btn-learn" href="https://github.com/Dhruv34788" target="_blank" rel="noopener noreferrer" style = {{color:"white"}}>View Projects <i className="icon-briefcase3" /></a></p>
+                          <p ><a className="btn btn-primary btn-learn" href="https://github.com/JoseSM1598" target="_blank" rel="noopener noreferrer" style = {{color:"white"}}>View Projects <i className="icon-briefcase3" /></a></p>
                         </div>
                       </div>
                     </div>
@@ -45,8 +45,8 @@ export default class Introduction extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>I often <br/>Write ... </h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://medium.com/@dhruv.barochia34788" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
+                          <h1>Check me out on <br/>Medium </h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://medium.com/@josesm919" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
                         </div>
                       </div>
                     </div>
