e997c89f67b66a7d748b53e2a219f3513479624c
js665 <js665@duke.edu>
2019-09-20 18:04:09 -0400

    small changes

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -94,21 +94,21 @@ export default class Projects extends Component {
 									</div>
 								</div>
 							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
-								<div className="project" style={{backgroundImage: 'url(images/blog-1.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Work 06</a></h3>
-											<span>Web Design</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
+							{/*<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">*/}
+							{/*	<div className="project" style={{backgroundImage: 'url(images/blog-1.jpg)'}}>*/}
+							{/*		<div className="desc">*/}
+							{/*			<div className="con">*/}
+							{/*				<h3><a href="work.html">Work 06</a></h3>*/}
+							{/*				<span>Web Design</span>*/}
+							{/*				<p className="icon">*/}
+							{/*					<span><a href="#"><i className="icon-share3" /></a></span>*/}
+							{/*					<span><a href="#"><i className="icon-eye" /> 100</a></span>*/}
+							{/*					<span><a href="#"><i className="icon-heart" /> 49</a></span>*/}
+							{/*				</p>*/}
+							{/*			</div>*/}
+							{/*		</div>*/}
+							{/*	</div>*/}
+							{/*</div>*/}
 						</div>
 						{/*<div className="row">*/}
 						{/*	<div className="col-md-12 animate-box">*/}
